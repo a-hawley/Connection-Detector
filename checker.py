db24061273b03a61a672635781b27e9d7132613f
Alec Hawley <alech35@yahoo.com>
2022-05-19 12:44:52 -0500

    made app functional

---
+++ checker.py
@@ -0,0 +1,19 @@
+from http.client import HTTPConnection
+from urllib.parse import urlparse
+
+
+def online(url, timeout=3):
+    error = Exception('unknown error')
+    parser = urlparse(url)
+    host = parser.netloc or parser.path.split('/')[0]
+
+    for port in (80, 443):
+        connection = HTTPConnection(host=host, port=port, timeout=timeout)
+        try:
+            connection.request('HEAD', '/')
+            return True
+        except Exception as e:
+            error = e
+        finally:
+            connection.close()
+    raise error
